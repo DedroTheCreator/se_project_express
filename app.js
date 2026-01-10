11f1e7d210013d5c77c8b1b431fca7ab2f665684
Phils Builds <darriuswilliams02@gmail.com>
2026-01-10 18:54:21 -0500

    New update

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  req.user = { _id: "5d8b8592978f8bd833ca8133" };
+  next();
+});
+
 // Routes
 app.use(routes);
 
