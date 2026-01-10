cbe4ca535b5055d9760a7a706c7f69a3fab0d0ff
Phils Builds <darriuswilliams02@gmail.com>
2026-01-10 14:00:19 -0500

    New update for my project

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ const auth = require("./middlewares/auth");
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
   .then(() => console.log("Connected to DB"))
-  .catch((err) => console.error("DB error", err));
+  .catch((err) => {
+    console.error("DB error", err);
+  });
 
 app.use(cors());
 app.use(express.json());
