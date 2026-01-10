83c0228ebdd0a420f85e171cebb46d718a05571e
Phils Builds <darriuswilliams02@gmail.com>
2026-01-10 13:55:36 -0500

    New update for my project

--- app.js
+++ app.js
@@ -5,19 +5,30 @@ const cors = require("cors");
 const { PORT = 3001 } = process.env;
 const app = express();
 
+const routes = require("./routes");
+const auth = require("./middlewares/auth");
+
+// Connect to MongoDB
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
-  .then(() => {
-    console.log("Connected to DB");
-  })
-  .catch((err) => {
-    console.error("DB error", err);
-  });
-
-const routes = require("./routes");
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.error("DB error", err));
 
 app.use(cors());
 app.use(express.json());
+
+app.use((req, res, next) => {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ")
+  ) {
+    return auth(req, res, next);
+  }
+
+  req.user = { _id: "5d8b8592978f8bd833ca8133" };
+  next();
+});
+
 app.use(routes);
 
 app.listen(PORT, () => {
