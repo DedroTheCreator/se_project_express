398d313dad51cfcc54adf69682ef68f7cdcab2c2
Phils Builds <darriuswilliams02@gmail.com>
2026-01-10 19:01:03 -0500

    new update

--- app.js
+++ app.js
@@ -9,15 +9,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Always set test user for all requests
 app.use((req, res, next) => {
   req.user = { _id: "5d8b8592978f8bd833ca8133" };
   next();
 });
 
-// Routes
+// Debug root: ensure server responds right away (for wait-port)
+app.get("/", (req, res) => {
+  res.send("ok");
+});
+
 app.use(routes);
 
-// Universal error handler (MUST have 4 arguments)
+// Error handler (4 args)
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode).json({
@@ -27,10 +32,21 @@ app.use((err, req, res, next) => {
   next();
 });
 
-// DB connection & server start
-mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-// eslint-disable-next-line no-console
-console.log(`App listening at port ${PORT}`);
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      // eslint-disable-next-line no-console
+      console.log(`App listening at port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    // eslint-disable-next-line no-console
+    console.error("DB connection error:", err);
+    process.exit(1);
+  });
